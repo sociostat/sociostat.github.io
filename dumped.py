15a5866222d13681febeab3f68c1f609414a90f8
Himanshu <harora940@decision-tree.com>
2020-04-19 20:50:14 +0530

    dumped

---
+++ dumped.py
@@ -0,0 +1,43 @@
+
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import twitter_samples, stopwords
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
+from nltk import FreqDist, classify, NaiveBayesClassifier
+import joblib
+import re, string, random
+
+import joblib
+classifier = joblib.load('text_analyzer')
+custom_tweet = input('Enter your text here: ')
+
+
+
+def remove_noise(tweet_tokens, stop_words = ()):
+
+    cleaned_tokens = []
+
+    for token, tag in pos_tag(tweet_tokens):
+        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
+                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+','', token)
+        token = re.sub("(@[A-Za-z0-9_]+)","", token)
+
+        if tag.startswith("NN"):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
+
+        lemmatizer = WordNetLemmatizer()
+        token = lemmatizer.lemmatize(token, pos)
+
+        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
+            cleaned_tokens.append(token.lower())
+    return cleaned_tokens
+
+
+
+custom_tokens = remove_noise(word_tokenize(custom_tweet))
+
+print(custom_tweet, classifier.classify(dict([token, True] for token in custom_tokens)))
